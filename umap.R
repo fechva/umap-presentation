c70b8f0d5d54be7509e305279125e1020d0ebf43
fechva <fechva@utu.fi>
2020-03-13 13:33:10 +0200

    Added details.

--- umap.R
+++ umap.R
@@ -1,4 +1,4 @@
-library("umap")
+library("umap") # requires certain python packages, see https://github.com/lmcinnes/umap
 library("tidyverse")
 
 View(iris)
@@ -6,12 +6,12 @@ View(iris)
 data <- as.matrix( iris[, c(1:4)] )
 
 seq.umap <- umap::umap(
-  d            = data,
-  method       = "umap-learn",
-  metric       = "euclidean",
-  n_neighbors  = 15,
-  min_dist     = 0.1,
-  n_components = 2)
+  d            = data, # this has to be a matrix
+  method       = "umap-learn", # makes sure we're using the full python implementation
+  metric       = "euclidean", # try different metrics! see below
+  n_neighbors  = 15, # default is 15, experiment!
+  min_dist     = 0.1, # default is 0.1, experiment!
+  n_components = 2) # dimension of the final projection
 
 umap_coordinates <- as.data.frame(seq.umap$layout)
 
